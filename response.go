f40182720b7cab19e8be783db74dcf30c4438eab
ppalone <pranjalalone@gmail.com>
2024-05-15 00:28:51 +0530

    add response types for client

--- response.go
+++ response.go
@@ -1,4 +1,24 @@
 package ytsearch
 
+// Video info
+type VideoInfo struct {
+	VideoID    string
+	Title      string
+	Channel    string
+	Thumbnails []Thumbnail
+	Views      string
+	Duration   string
+}
+
+// Thumbnail
+type Thumbnail struct {
+	URL    string
+	Width  uint
+	Height uint
+}
+
 // Search Response
-type SearchResponse struct{}
+type SearchResponse struct {
+	Results      []VideoInfo
+	Continuation string
+}
