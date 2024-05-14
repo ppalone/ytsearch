ca7fa0b8a6768be02c46310ee89168a3cc58d1fe
ppalone <pranjalalone@gmail.com>
2024-05-15 00:18:38 +0530

    remove raw youtube response

--- response.go
+++ response.go
@@ -1,8 +1,4 @@
 package ytsearch
 
-// Raw YouTube Search Response
-// Will be received from YouTube's internal API
-type rawYTSearchResponse struct{}
-
 // Search Response
 type SearchResponse struct{}
