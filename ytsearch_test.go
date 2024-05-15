79160e307e2598c903e071806113c85b9cdd1b93
Pranjal Alone <pranjalalone@gmail.com>
2024-05-15 23:49:14 +0530

    add test for SearchWithContext

--- ytsearch_test.go
+++ ytsearch_test.go
@@ -1,7 +1,9 @@
 package ytsearch
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -18,4 +20,38 @@ func Test_Search(t *testing.T) {
 
 	// continuation key is not empty
 	assert.NotEmpty(t, res.Continuation)
+}
+
+func Test_SearchWithContext(t *testing.T) {
+	t.Run("context with insufficent timeout", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*1)
+
+		// client
+		c := Client{}
+		res, err := c.SearchWithContext(ctx, "nocopyrightsounds")
+
+		assert.Error(t, err, context.Canceled)
+		assert.Empty(t, res.Results)
+
+		// clean up
+		t.Cleanup(func() {
+			cancel()
+		})
+	})
+
+	t.Run("context with sufficent timeout", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+
+		// client
+		c := Client{}
+		res, err := c.SearchWithContext(ctx, "nocopyrightsounds")
+
+		assert.NoError(t, err)
+		assert.NotEmpty(t, res.Results)
+
+		// clean up
+		t.Cleanup(func() {
+			cancel()
+		})
+	})
 }
