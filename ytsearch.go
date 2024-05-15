fd97ce3fb51a88dbbc044590449268f9964881db
Pranjal Alone <pranjal.alone@logicmonitor.com>
2024-05-15 23:31:29 +0530

    fix return types for client

--- ytsearch.go
+++ ytsearch.go
@@ -9,18 +9,18 @@ const (
 // YouTube Search Client
 type Client struct{}
 
-func (c *Client) Search(query string) ([]SearchResponse, error) {
-	return []SearchResponse{}, nil
+func (c *Client) Search(query string) (SearchResponse, error) {
+	return SearchResponse{}, nil
 }
 
-func (c *Client) SearchWithContext(ctx context.Context, query string) ([]SearchResponse, error) {
-	return []SearchResponse{}, nil
+func (c *Client) SearchWithContext(ctx context.Context, query string) (SearchResponse, error) {
+	return SearchResponse{}, nil
 }
 
-func (c *Client) Next(key string) ([]SearchResponse, error) {
-	return []SearchResponse{}, nil
+func (c *Client) Next(key string) (SearchResponse, error) {
+	return SearchResponse{}, nil
 }
 
-func (c *Client) NextWithContext(ctx context.Context, key string) ([]SearchResponse, error) {
-	return []SearchResponse{}, nil
+func (c *Client) NextWithContext(ctx context.Context, key string) (SearchResponse, error) {
+	return SearchResponse{}, nil
 }
