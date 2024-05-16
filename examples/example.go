1db81f899b9592c58b09a6c4c941f61ba7d5b341
Pranjal Alone <pranjalalone@gmail.com>
2024-05-17 02:04:49 +0530

    add Next example

--- examples/example.go
+++ examples/example.go
@@ -16,7 +16,23 @@ func main() {
 		panic(err)
 	}
 
+	fmt.Println("Search Results:")
+
 	// res.Results contains an array of Search results
+	for _, res := range res.Results {
+		fmt.Println("Video Title:", res.Title, "Channel:", res.Channel)
+	}
+
+	fmt.Println("Next Search Results:")
+
+	// res.Continuation contains continuation token
+	// can be used to fetch next results
+	// since youtube limits the number of search results
+	res, err = c.Next(res.Continuation)
+	if err != nil {
+		panic(err)
+	}
+
 	for _, res := range res.Results {
 		fmt.Println("Video Title:", res.Title, "Channel:", res.Channel)
 	}
